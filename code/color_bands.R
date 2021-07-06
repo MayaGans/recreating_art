0c9b92ecef4e72adea86b9dc523987a579d4d0a5
Maya Gans <jaffe.maya@gmail.com>
2021-07-06 15:24:58 -0700

    add code

--- code/color_bands.R
+++ code/color_bands.R
@@ -41,7 +41,7 @@ square <- geom_rect(data = data.frame(xmin = 0, ymin = 0, xmax = 1, ymax = -1),
 
 
 # ------------------------
-# Plot 1
+# Plate 1
 # ------------------------
 
 left <- purrr::map2(seq(-0.99,-0.04, length.out = 32), 
@@ -75,13 +75,13 @@ outline <- data.frame(
   purrr::pmap(., ~geom_segment(aes(x = ..1, y = ..2, xend = ..3, yend = ..4), size = 3))
 
 
-ggplot() +
+p1 <- ggplot() +
   rev(bottom) + left + right + outline + square +
   coord_fixed(clip = "on", ylim=c(-1,0), xlim=c(0,1), expand = FALSE) +
   theme_void()
 
 # ------------------------
-# Plot 2
+# Plate 2
 # ------------------------
 
 top_right <- purrr::map2(seq(10,160, length.out = 30), 
@@ -112,7 +112,7 @@ diagonal <- function(a, color) {
   geom_segment(aes(x = 0.5, y = -(a), xend = a, yend = -0.5), size = 3, color = colors[color])
 }
 
-ggplot() +
+p2 <- ggplot() +
   rev(bottom_left) +
   rev(top_right) +
   bottom_right +
@@ -124,7 +124,7 @@ ggplot() +
   theme_void()
 
 # ------------------------
-# Plot 3
+# Plate 3
 # ------------------------
 
 top_right <- purrr::map2(seq(10,200, length.out = 40), 
@@ -146,7 +146,7 @@ bottom <- purrr::map2(seq(10,175, length.out = 30),
                                        size = .x))
 
 
-ggplot() +
+p3 <- ggplot() +
   rev(bottom) +
   rev(top_left) +
   rev(top_right) +
@@ -155,21 +155,224 @@ ggplot() +
   theme_void()
 
 # ------------------------
-# Plot 4
+# Plate 4
 # ------------------------
 
-# ------------------------
-# Plot 5
-# ------------------------
+right <- purrr::map2(seq(10,200, length.out = 40), 
+                         sample_no_repeats(names(colors), 40),
+                         ~geom_point(data = data.frame(x = 1, y = -0.5), 
+                                     aes(x,y, color = colors[.y]), 
+                                     size = .x))
+
+top_left <- purrr::map2(seq(10,400, length.out = 70), 
+                        sample_no_repeats(names(colors), 70),
+                        ~geom_point(data = data.frame(x = 0, y = 0), 
+                                    aes(x,y, color = colors[.y]), 
+                                    size = .x))
+
+bottom_left <- purrr::map2(seq(10,400, length.out = 70), 
+                      sample_no_repeats(names(colors), 70),
+                      ~geom_point(data = data.frame(x = 0.5, y = -1), 
+                                  aes(x,y, color = colors[.y]), 
+                                  size = .x))
+
+
+
+p41 <- ggplot() +
+  rev(right) +
+  square +
+  geom_vline(xintercept = 0.5, size = 3) +
+  coord_fixed(clip = "on", ylim=c(-1,0), xlim=c(0.5,1), expand = FALSE) +
+  theme_void()
+
+p42 <- ggplot() +
+  rev(top_left) +
+  square +
+  geom_vline(xintercept = 0.5, size = 3) +
+  geom_hline(yintercept = -0.5, size = 3) +
+  coord_fixed(clip = "on", ylim=c(-0.5,0), xlim=c(0,0.5), expand = FALSE) +
+  theme_void()
+
+p43 <- ggplot() +
+  rev(bottom_left) +
+  square +
+  geom_vline(xintercept = 0.5, size = 3) +
+  geom_hline(yintercept = -0.5, size = 3) +
+  coord_fixed(clip = "on", ylim=c(-1,-0.5), xlim=c(0,0.5), expand = FALSE) +
+  theme_void()
+
+lay <- rbind(c(1,3),
+             c(2,3))
+
+p4 <- grid.arrange(grobs = list(p42, p43, p41), 
+             layout_matrix = lay,
+             vp = viewport(x = 0.5, y = 0.5)
+)
 
 # ------------------------
-# Plot 6
+# Plate 5
 # ------------------------
 
-# ------------------------
-# Plot 7
-# ------------------------
+top <- purrr::pmap(
+  list(
+    seq(0, 1, by = 0.025),
+    sample_no_repeats(names(colors), 41)
+  ),
+  ~geom_segment(aes(x = ..1, y = 0, yend = -0.3, xend = ..1), size = 3, color = colors[..2])
+)
+
+bottom_left <- purrr::map2(seq(10,300, length.out = 50), 
+                         sample_no_repeats(names(colors), 50),
+                         ~geom_point(data = data.frame(x = 0.5, y = -0.3), 
+                                     aes(x,y, color = colors[.y]), 
+                                     size = .x))
+
+bottom_right <- purrr::pmap(
+  list(
+    seq(0.02, 1, by = 0.02),
+    sample_no_repeats(names(colors), 50)
+  ),
+  ~geom_arc(aes(x0 = 0.49, y0 = -1, start = pi*0.01, end = pi*0.6, r = ..1), size = 3, color = colors[[..2]])
+)
+
+ggplot() +
+  # cheat and use circles
+  rev(bottom_left) +
+  bottom_right + 
+  top + 
+  # fill in the rest manually
+  geom_hline(yintercept = -0.3, size = 3) +
+  geom_segment(aes(x = 0.5, y = -0.3, xend = 0.5, yend = -1), size = 3) +
+  square + 
+  coord_fixed(clip = "on", ylim=c(-1,0), xlim=c(0,1), expand = FALSE) +
+  theme_void() 
+
 
 # ------------------------
-# Plot 8
+# Plate 6
 # ------------------------
+
+left_segment <- purrr::pmap(
+  list(
+    rev(seq(-1, -0.44, by = 0.04)),
+    rev(seq(-1, 0.325, by = 0.025))[1:15],
+    sample_no_repeats(names(colors), 15)
+  ), ~ geom_segment(aes(x = 0, y = ..1, xend = ..2, yend = -1), size = 3, color = colors[..3])
+)
+
+right_segment <- purrr::pmap(
+  list(
+    rev(seq(-1, -0.44, by = 0.04)),
+    seq(0.675, 1, by = 0.025)[1:15],
+    sample_no_repeats(names(colors), 15)
+  ), ~ geom_segment(aes(x = 1, y = ..1, xend = ..2, yend = -1), size = 3, color = colors[..3])
+)
+
+p6 <- ggplot() +
+  purrr::map2(rev(seq(0.1, 1, by= 0.05)), sample_no_repeats(names(colors), 19),
+              ~geom_regon(aes(x0 = 0.5, y0 = -0.3, sides = 3, angle = pi, r = .x), fill = colors[.y])) +
+  left_segment +
+  right_segment +
+  square +
+  coord_fixed(clip = "on", ylim=c(-1,0), xlim=c(0,1), expand = FALSE) +
+  theme_void()
+  
+
+# ------------------------
+# Plate 7
+# ------------------------
+
+left <- purrr::map2(seq(-2,-0.04, length.out = 70), 
+                    sample_no_repeats(names(colors), 70),
+                    ~geom_abline(intercept = .x, slope = 1, size = 3, color = colors[.y]))
+
+top_right <- purrr::map2(seq(10,150, length.out = 30), 
+                         sample_no_repeats(names(colors), 30),
+                         ~geom_point(data = data.frame(x = 1, y = 0), 
+                                     aes(x,y, color = colors[.y]), 
+                                     size = .x))
+
+bottom_right <- purrr::map2(seq(10,160, length.out = 25), 
+                         sample_no_repeats(names(colors), 25),
+                         ~geom_point(data = data.frame(x = 0.66, y = -1), 
+                                     aes(x,y, color = colors[.y]), 
+                                     size = .x))
+middle <- purrr::pmap(
+  list(
+    seq(0.33, 0.66, by = 0.025),
+    sample_no_repeats(names(colors), 14)
+  ),
+  ~geom_segment(aes(x = ..1, y = 0, yend = -1, xend = ..1), size = 3, color = colors[..2])
+)
+
+bottom_right <- purrr::pmap(
+  list(
+    seq(0.02, 0.5, by = 0.02),
+    sample_no_repeats(names(colors), 25)
+  ),
+  ~geom_arc(aes(x0 = 0.59, y0 = -1, start = pi*0.01, end = pi*0.6, r = ..1), size = 3, color = colors[[..2]])
+)
+
+p7 <- ggplot() +
+  left + 
+  # cheat and use circles
+  rev(top_right) +
+  # arcs
+  bottom_right +
+  # fill in the rest manually
+  geom_arc(aes(x0 = 0.59, y0 = -1, start = pi*0.01, end = pi*0.6, r = 0.5), size = 3, color = colors[[sample_no_repeats(names(colors), 1)]]) +
+  geom_arc(aes(x0 = 0.59, y0 = -1, start = pi*0.1, end = pi*0.6, r = 0.52), size = 3, color = colors[[sample_no_repeats(names(colors), 1)]]) +
+  geom_arc(aes(x0 = 0.59, y0 = -1, start = pi*0.12, end = pi*0.6, r = 0.54), size = 3, color = colors[[sample_no_repeats(names(colors), 1)]]) +
+  geom_arc(aes(x0 = 0.59, y0 = -1, start = pi*0.145, end = pi*0.6, r = 0.56), size = 3, color = colors[[sample_no_repeats(names(colors), 1)]]) +
+  geom_arc(aes(x0 = 0.59, y0 = -1, start = pi*0.166, end = pi*0.6, r = 0.58), size = 3, color = colors[[sample_no_repeats(names(colors), 1)]]) +
+  geom_arc(aes(x0 = 0.59, y0 = -1, start = pi*0.185, end = pi*0.6, r = 0.6), size = 3, color = colors[[sample_no_repeats(names(colors), 1)]]) +
+  geom_arc(aes(x0 = 0.59, y0 = -1, start = pi*0.2, end = pi*0.6, r = 0.62), size = 3, color = colors[[sample_no_repeats(names(colors), 1)]]) +
+  middle +
+  square +
+  geom_vline(xintercept = 0.33, size = 3) +
+  geom_segment(aes(x = 0.66, y = -0.5, yend = -0.5, xend = 1), size = 3) +
+  geom_vline(xintercept = 0.66, size = 3) +
+  coord_fixed(clip = "on", ylim=c(-1,0), xlim=c(0,1), expand = FALSE) +
+  theme_void() 
+
+# ------------------------
+# Plate 8
+# ------------------------
+
+bottom_right <- purrr::map2(seq(10,200, length.out = 30), 
+                           sample_no_repeats(names(colors), 30),
+                           ~geom_point(data = data.frame(x = 0.3, y = -1), 
+                                       aes(x,y, color = colors[.y]), 
+                                       size = .x))
+
+top_right <- purrr::pmap(
+  list(
+    seq(0.3, 1, by = 0.02),
+    sample_no_repeats(names(colors), 36)
+  ),
+  ~geom_segment(aes(x = ..1, y = 0, yend = -0.75, xend = ..1), size = 3, color = colors[..2])
+)
+
+left <- purrr::pmap(
+  list(
+    seq(-1, -0.02, by = 0.02),
+    sample_no_repeats(names(colors), 50)
+  ),
+  ~geom_segment(aes(x = 0, y = ..1, yend = ..1, xend = 0.3), size = 3, color = colors[..2])
+)
+
+p8 <- ggplot() +
+  rev(bottom_right) + 
+  top_right +
+  left +
+  square + 
+  geom_vline(xintercept = 0.3, size = 3) +
+  geom_segment(aes(x = 0.3, y = -0.75, yend = -0.75, xend = 1), size = 3) +
+  coord_fixed(clip = "on", ylim=c(-1,0), xlim=c(0,1), expand = FALSE) +
+  theme_void()
+
+
+all <- (p1 | p2 | p3 | p4) / (p5 | p6 | p7 | p8) &
+  theme(plot.margin = margin(5,5,5,5))
+
+ggsave("color_bands.png", all, width = 14, height = 7)
