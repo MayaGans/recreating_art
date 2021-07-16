ffb9a078b5ced9ebc9bf8f89627545d52ed6949e
Maya Gans <jaffe.maya@gmail.com>
2021-07-16 10:33:41 -0700

    add exact

--- code/phantoms_shadow.R
+++ code/phantoms_shadow.R
@@ -5,8 +5,8 @@
 
 colors <- list(
   teal = "#258B97",
-  peach = "#DA8873",
-  yellow = "#ECA81F",
+  peach = "#DA8873", 
+  yellow = "#ECA81F", 
   blue = "#024E98"
 )
 
@@ -30,23 +30,28 @@ point_top_left <- point_factory(0,0.5,1,0,1,0.75,0,0)
 point_bottom_right <- point_factory(0,0.5,1,1,1,0.25,0,1)
 point_bottom_left <- point_factory(0,0,0.5,1,1,0,0.25,1)
 
+points <- c(point_top_left,
+            point_bottom_left,
+            point_top_right,
+            point_bottom_right)
+
+
 # -----------------------------
-# Generative Art
+# 8x8 Data
 # -----------------------------
 
 all_x = rep(seq(0,7), 8)
 all_y = rep(0:7, each = 8)
 ids = 1:length(all_x)
 
-points <- c(point_top_left,
-  point_bottom_left,
-  point_top_right,
-  point_bottom_right)
+# -----------------------------
+# Generative Art
+# -----------------------------
 
 meta_df <- tibble(
   funcs = rep(sample(points, 8*8, replace = TRUE)),
   id = ids,
-  sample(unlist(colors), 8*8, replace = TRUE),
+  sample_no_repeats(unlist(colors), 8*8),
   all_x = all_x,
   all_y = all_y,
 )
@@ -56,6 +61,117 @@ positions <- purrr::pmap(meta_df, ~rlang::exec(..1,..2,..3,..4,..5)) %>%
 
 ggplot(positions, aes(x = x, y = y)) +
   geom_polygon(aes(group = id), fill=positions$value) +
+  theme_void() +
+  guides(fill = FALSE) +
+  theme(plot.background = element_rect(color = "#9BA292", fill = "#9BA292"))
+
+
+# -------------------
+# Exact Reproduction
+# -------------------
+
+meta_df <- tibble(
+  funcs = c(
+    point_top_right,
+    point_bottom_right,
+    point_bottom_left,
+    point_top_left,
+    point_bottom_left,
+    point_top_left,
+    point_top_right,
+    point_bottom_right,
+    
+    point_top_left,
+    point_bottom_left,
+    point_bottom_right,
+    point_top_right,
+    point_bottom_right,
+    point_top_right,
+    point_top_left,
+    point_bottom_left,
+    
+    point_bottom_left,
+    point_top_left,
+    point_top_right,
+    point_bottom_right,
+    point_top_right,
+    point_bottom_right,
+    point_bottom_left,
+    point_top_left,
+    
+    point_bottom_right,
+    point_top_right,
+    point_top_left,
+    point_bottom_left,
+    point_top_left,
+    point_bottom_left,
+    point_bottom_right,
+    point_top_right,
+    
+    point_bottom_left,
+    point_top_left,
+    point_top_right,
+    point_bottom_right,
+    point_top_right,
+    point_bottom_right,
+    point_bottom_left,
+    point_top_left,
+    
+    point_bottom_right,
+    point_top_right,
+    point_top_left,
+    point_bottom_left,
+    point_top_left,
+    point_bottom_left,
+    point_bottom_right,
+    point_top_right,
+    
+    point_top_right,
+    point_bottom_right,
+    point_bottom_left,
+    point_top_left,
+    point_bottom_left,
+    point_top_left,
+    point_top_right,
+    point_bottom_right,
+    
+    point_top_left,
+    point_bottom_left,
+    point_bottom_right,
+    point_top_right,
+    point_bottom_right,
+    point_top_right,
+    point_top_left,
+    point_bottom_left
+  ),
+  id = ids,
+  colors = c(
+    "teal","peach","yellow","blue",
+    "peach","blue","teal","yellow",
+    "yellow","blue","teal","peach",
+    "teal","yellow","peach","blue",
+    "peach","teal","yellow","teal",
+    "blue","peach","teal","peach",
+    "blue","yellow","blue","peach",
+    "yellow","teal","yellow","blue",
+    "peach","blue","teal","yellow",
+    "blue","yellow","peach","teal",
+    "teal","yellow","peach","blue",
+    "peach","teal","blue","yellow",
+    "blue","peach","teal","peach",
+    "yellow","blue","peach","blue",
+    "yellow","teal","yellow","blue",
+    "teal","peach","teal","yellow"
+  ),
+  all_x = all_x,
+  all_y = all_y,
+)
+
+positions <- purrr::pmap(meta_df, ~rlang::exec(..1,..2,..3,..4,..5)) %>%
+  purrr::reduce(dplyr::bind_rows)
+
+ggplot(positions, aes(x = x, y = y)) +
+  geom_polygon(aes(group = id), fill=colors[positions$value]) +
   theme_void() +
   guides(fill = FALSE) +
   theme(plot.background = element_rect(color = "#9BA292", fill = "#9BA292"))
